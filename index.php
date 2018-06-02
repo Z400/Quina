41ebd14522ed649cff42b3a28c5d08269a24e4ef
Viana <matheusgoldship@gmail.com>
2018-06-01 22:49:21 -0300

    Index

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Quina</title>
+    <link rel="stylesheet" href="est.css">
+  </head>
+  <body>
+<div class="quina">
+
+<input type="text" name="" value="">
+<input type="submit" name="" value="Jogar">
+</div>
+
+
+
+
+  </body>
+</html>
